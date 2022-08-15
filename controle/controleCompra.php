45385d38b56ecfe1993eecbd2a6b865a88734107
lipestaub <lipestaub@gmail.com>
2022-08-15 17:30:02 -0300

    .

--- controle/controleCompra.php
+++ controle/controleCompra.php
@@ -10,7 +10,27 @@ if (!isset($_SESSION['perfil'])) {
 }
 
 $idProduto = $_GET['p'];
-    
-echo $idProduto;
+
+if (!isset($_SESSION['idPedido'])) {
+    $resposta = mysqli_query($conexao, "SELECT MAX(id) FROM pedido");
+
+    while ($dados = mysqli_fetch_array($resposta)) {
+        $idPedido = $dados[0];
+    }
+
+    if (is_null($idPedido)) {
+        $idPedido = 1;
+    }
+    else {
+        $idPedido++;
+    }
+
+    echo $idPedido;
+    $idPessoa = $_SESSION['id'];
+    $data = time();
+
+    mysqli_query($conexao, "INSERT INTO pedido (idPedido, idPessoa, data, status) VALUES ($idPedido, $idPessoa, $data, 0)");
+
+}
 
 // echo "<script>window.location.href = '../index.php';</script>";
